3656a0881617353c18f978b8bc22d22999db698c
it21273094 <it21273094@my.sliit.lk>
2023-05-02 12:09:59 +0530

    company form

---
+++ app/src/main/java/com/example/job_portal/CompanyData.kt
@@ -0,0 +1,5 @@
+package com.example.job_portal
+
+data class CompanyData ( val companyName:String?=null,val title:String?=null,
+                         val category:String?=null,val type:String?=null,val salary:String?=null,
+                         val description:String?=null)
